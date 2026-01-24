a1ba49126e13791f0a29785913f14ee22da21cd3
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2026-01-24 17:51:31 -0500

    build 95ff757e87797cbaef8675a1fbb59f7e93b62f06

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1157557975"
+    serviceWorkerVersion: "752845155"
   }
 });
